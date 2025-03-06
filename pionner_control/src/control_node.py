62c88713649f35f09bb4d7899b2d86e275c47797
Jonas Moreira <jonasmoreira076@gmail.com>
2025-03-06 13:05:44 -0300

    rename arquivo pioneer_control

--- pionner_control/src/control_node.py
+++ pionner_control/src/control_node.py
@@ -3,7 +3,7 @@ import rospy
 from geometry_msgs.msg import Twist
 
 def move_robot(linear=0.0, angular=0.0):
-    rospy.init_node("pioneer_control")
+    rospy.init_node("pionner_control")
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     rate = rospy.Rate(10)
 
